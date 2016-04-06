99c5557b5dc76419477953517fab08efc6be45a8
AbstractClass <connor.macleod96@hotmail.ca>
2016-04-05 23:02:11 -0400

    rechain 2 complete

--- rechain2.py
+++ rechain2.py
@@ -1,26 +1,26 @@
 import subprocess
 
 
-def to_cluster(starting_sector, sector, sector_size=512, cluster_size=4096):
-    cluster = ((sector - starting_sector) / (cluster_size / sector_size)) + 2
-    try:
-        return int(cluster)
-    except TypeError:
-        return "Incorrect Arguments, calculated a float, should be int"
+def to_int_list(value, split_char):
+    return list(map(int, value.split(split_char)))
+
 
 img_path = "/home/connor/Documents/SPR401/FAT-fs.dd"
 
+
 cmd =["fsstat", img_path]
+###################################################################################
 # Proper method
 # call = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 # out, err = call.communicate()
+###################################################################################
 
 # Kinda hacky
 call = subprocess.check_output(cmd)
 
 fsstat = call.decode('utf-8').splitlines()
 
-important_info = ("File System Type", "FAT 0", "*** Root Directory", "Sector Size", "Cluster Size")
+important_info = ("File System Type", "FAT 0", "FAT 1","*** Root Directory", "Sector Size", "Cluster Size")
 
 fsstat_important = set()
 for x in fsstat:
@@ -30,8 +30,9 @@ for x in fsstat:
 fs_attributes = {n[0].strip(): n[1].strip() for n in list(map(lambda x: x.split(": "), fsstat_important))}
 
 # Convert root directory and FAT table sectors to list of ints
-fs_attributes['*** Root Directory'] = list(map(int, fs_attributes['*** Root Directory'].split(" - ")))
-fs_attributes['* FAT 0'] = list(map(int, fs_attributes['* FAT 0'].split(" - ")))
+fs_attributes['*** Root Directory'] = to_int_list(fs_attributes['*** Root Directory'], ' - ')
+fs_attributes['* FAT 0'] = to_int_list(fs_attributes['* FAT 0'], ' - ')
+fs_attributes['* FAT 1'] = to_int_list(fs_attributes['* FAT 1'], ' - ')
 
 # Int Conversion
 for key in fs_attributes:
@@ -44,20 +45,61 @@ for key in fs_attributes:
 bs = str(fs_attributes['Sector Size'])
 skip = str(fs_attributes['* FAT 0'][0])
 count = str(fs_attributes['* FAT 0'][1] - fs_attributes['* FAT 0'][0])
+of = "FAT_Table_0.dd"
 
-cmd = ['dd', 'if='+img_path, 'bs='+bs, 'skip='+skip, 'count='+count]
+cmd = ['dd', 'if='+img_path, 'bs='+bs, 'skip='+skip, 'count='+count, 'of='+of]
+dd = subprocess.Popen(args=cmd)
 
-dd = subprocess.Popen(args=cmd, stdout=subprocess.PIPE)
-dd_hex = subprocess.check_output(('hexdump'), stdin=dd.stdout).decode('utf-8')
+if fs_attributes['File System Type Label'] == 'FAT32':
+    rs = 4
+elif fs_attributes['File System Type Label'] == 'FAT16':
+    rs = 2
+else:
+    print("FILESYSTEM IS NOT FAT!  PROGRAM WILL NOT RUN CORRECTLY OR IT WILL CRASH!")
+    print("read size (rs) is being set to -1 as punishment.")
+    rs = -1
+
+fat_table = []
+with open('FAT_Table_0.dd', 'rb') as table:
+    while True:
+        entry = table.read(rs)
+        if entry != b'':
+            fat_table.append(entry[::-1])
+        if not entry:
+            break
+
+entry = b'\x00\x00\x00\x00'
+while entry == b'\x00\x00\x00\x00':
+    fat_table.pop()
+    entry = fat_table[-1]
+
+alloc_clusters = []
+for cluster, entry in enumerate(fat_table):
+    value = "ERROR"
+    if entry == b'\x00\x00\x00\x00':
+        continue
+    elif entry == b'\x0f\xff\xff\xf8' or entry == b'\x0f\xff\xff\xff':
+        value = "EOF"
+    else:
+        value = int.from_bytes(entry, byteorder='big')
+
+    alloc_clusters.append([cluster, value])
+
+cluster_chains = [x for x in alloc_clusters if x[1] == 'EOF']
+cluster_pointers = [x for x in alloc_clusters if x[1] != 'EOF']
+
+for pointer in sorted(cluster_pointers, reverse=True):
+    for n in range(0, len(cluster_chains)):
+        if pointer[1] == cluster_chains[n][0]:
+            cluster_chains[n].insert(0, pointer[0])
 
 # Craft dd command for root directory
-skip = str(fs_attributes['*** Root Directory'][0])
-count = str(fs_attributes['*** Root Directory'][1] - fs_attributes['*** Root Directory'][0])
+# skip = str(fs_attributes['*** Root Directory'][0])
+# count = str(fs_attributes['*** Root Directory'][1] - fs_attributes['*** Root Directory'][0])
 
-cmd = ['dd', 'if='+img_path, 'bs='+bs, 'skip='+skip, 'count='+count]
+# cmd = ['dd', 'if='+img_path, 'bs='+bs, 'skip='+skip, 'count='+count]
 
 # dd = subprocess.Popen(args=cmd, stdout=subprocess.PIPE)
 # dd_hex = subprocess.check_output(('hexdump'), stdin=dd.stdout).decode('utf-8')
 
-print(fs_attributes)
-print(dd_hex)
+print(cluster_chains)
